e27d80b82131cdc6e82210f00ca32baa46992af2
zenmustafaa <67698202+zenmustafaa@users.noreply.github.com>
2020-11-27 10:30:11 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Ahmad Zen Mustafa','iIkhlasul Amal','Muhammad Zeidar'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function tes(){
+ 
+if(count == txtElement.length){
+    count = 0;
+}
+
+currentTxt = txtElement[count];
+
+words = currentTxt.slice(0, ++txtIndex);
+document.querySelector('.efek-tes').textContent = words;
+
+setTimeout(tes,500);
+
+if(words.length == currentTxt.length){
+    count++;
+    txtIndex = 0;
+}
+
+})();
